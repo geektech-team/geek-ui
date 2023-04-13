8dc9c617bc8fd24928b98075115acd8d7274d022
Jimmie Max <mayiming@morefungroup.com>
2023-04-13 17:15:16 +0800

    chore(global): lint es

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   // '*.{css,less,scss,jsx}': 'stylelint --fix --custom-syntax postcss-less',
+  '*.{js,jsx,ts,vue,css,less,json,md}': ['prettier --write'],
   '*.{js,ts,vue}': ['eslint --fix'],
   '*.{css,less}': ['stylelint --fix --custom-syntax postcss-less'],
-  '*.{js,jsx,ts,vue,css,less,json,md}': ['prettier --write'],
 };
