169f120550d970e12977671ade1aeaf82ed6ec35
Jimmie Max <mayiming@morefungroup.com>
2023-04-14 15:41:00 +0800

    chore(global): version update

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  '*.{js,jsx,ts,vue,css,less,json,md}': ['prettier --write'],
-  '*.{js,ts,vue}': ['eslint --fix'],
-  '*.{css,less}': ['stylelint --fix --custom-syntax postcss-less'],
-};
